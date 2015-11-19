aad76f9624be10716305f89c684cf3ef25f995d2
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-11-19 16:17:47 -0200

    User creates a item with item category

--- spec/features/user_creates_a_new_inventory_item_spec.rb
+++ spec/features/user_creates_a_new_inventory_item_spec.rb
@@ -2,16 +2,20 @@ require 'rails_helper'
 
 feature 'User creates a new inventory item' do
   scenario 'successfully' do
+    item_category = ItemCategory.create(name: "Notebook")
+
     visit new_item_path
     fill_in 'Name', with: 'MacBook Air'
     fill_in 'Description', with: 'Apple Notebook'
     fill_in 'Features', with: '8gb, 500gb, 13"'
     fill_in 'Asset number', with: '134 MAC'
+    select item_category.name, from: 'Item category'
     click_button 'Create Item'
 
     expect(page).to have_content 'MacBook Air'
     expect(page).to have_content 'Apple Notebook'
     expect(page).to have_content '8gb, 500gb, 13"'
     expect(page).to have_content '134 MAC'
+    expect(page).to have_content item_category.name
   end
 end
