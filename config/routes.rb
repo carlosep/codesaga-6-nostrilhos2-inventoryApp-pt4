5307a88583ba31a71f6027d94234261056da51b9
Carlos E. Palha <carlao_epalha@hotmail.com>
2015-11-27 10:05:47 -0200

    Rails Intermediário - Parte 4 - Concluído

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :item_categories
+
   root 'home#index'
   resources :items, only: [:new, :create, :show]
   # The priority is based upon order of creation: first created -> highest priority.
